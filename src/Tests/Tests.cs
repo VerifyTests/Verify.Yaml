4b7f793b3114e16ac4d7002eae407326bec9abc8
Simon Cropp <simon.cropp@gmail.com>
2025-06-04 20:17:30 +1000

    .

--- src/Tests/Tests.cs
+++ src/Tests/Tests.cs
@@ -13,63 +13,18 @@ public class Tests
             msg: No action taken
         """;
 
-    [Test]
-    public Task TestJsonDocument()
+    YamlDocument document;
+    YamlStream yamlStream;
+
+    public Tests()
     {
         var input = new StringReader(yaml);
-        var yamlStream = new YamlStream();
+        yamlStream = new();
         yamlStream.Load(input);
-        return Verify(yamlStream);
+        document = yamlStream.Documents[0];
     }
 
     [Test]
-    public Task ScrubMember()
-    {
-        var input = new StringReader(yaml);
-        var yamlStream = new YamlStream();
-        yamlStream.Load(input);
-        return Verify(yamlStream).ScrubMember("error");
-    }
-    //
-    // [Test]
-    // public Task TestJsonElement() =>
-    //     Verify(JsonDocument.Parse(json).RootElement);
-    //
-    // [Test]
-    // public Task TestJsonNode() =>
-    //     Verify(JsonNode.Parse(json));
-    //
-    // [Test]
-    // public Task TestJsonObject() =>
-    //     Verify(JsonNode.Parse(json)!.AsObject());
-    //
-    // [Test]
-    // public Task NullValue() =>
-    //     Verify(
-    //             JsonDocument.Parse(
-    //                 """
-    //                 {
-    //                   "short": {
-    //                     "a": null,
-    //                     "error": "a"
-    //                   }
-    //                 }
-    //                 """))
-    //         .AddExtraSettings(
-    //             _ =>
-    //             {
-    //                 _.DefaultValueHandling = DefaultValueHandling.Include;
-    //                 _.NullValueHandling = NullValueHandling.Include;
-    //             });
-    //
-    // [Test]
-    // public Task Numbers() =>
-    //     Verify(
-    //         JsonDocument.Parse(
-    //             """
-    //             {
-    //                 "int": 1,
-    //                 "double": 4.4
-    //             }
-    //             """));
+    public Task Document() =>
+        Verify(document);
 }
